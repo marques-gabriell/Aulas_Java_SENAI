1bf8fbbf4d3d273970e964b1026945b3b94f8efd
Gabriel M <magicomnstr@gmail.com>
2026-07-10 21:39:18 -0300

    Criação da atividade 4

---
+++ src/atividades/Atividade2/atividade4.java
@@ -0,0 +1,38 @@
+package atividades.Atividade2;
+
+import java.util.Scanner;
+
+public class atividade4 {
+    public static void main(String[] args) {
+
+        Scanner entrada = new Scanner(System.in);
+
+
+        System.out.println("Informe seu peso?");
+        double peso = entrada.nextInt();
+
+        System.out.println(
+                "\nSeu peso é:" + peso
+        );
+
+        System.out.println("Informe sua altura?");
+        double altura = entrada.nextInt();
+
+        System.out.println(
+                "\nSua altura é:" + altura
+        );
+
+
+        double IMC = peso / (altura * altura);
+
+        System.out.println(
+                "\nSeu IMC é:" + IMC
+        );
+
+
+
+
+
+        entrada.close();
+    }
+}
