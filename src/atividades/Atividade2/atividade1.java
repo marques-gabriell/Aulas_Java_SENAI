0700aaade9056711e371045db4bd1d5266179779
Gabriel M <magicomnstr@gmail.com>
2026-07-10 21:39:04 -0300

    Criação da atividade 1

---
+++ src/atividades/Atividade2/atividade1.java
@@ -0,0 +1,33 @@
+package atividades.Atividade2;
+
+import java.util.Scanner;
+
+public class atividade1 {
+
+    public static void main(String[] args) {
+
+        Scanner entrada = new Scanner(System.in);
+
+
+        System.out.println("Informe sua idade?");
+        int idade = entrada.nextInt();
+
+
+
+        System.out.println(
+                "\nSuas informações:" +
+                        "\nIdade: " + idade
+        );
+
+        if ( idade < 16) {
+            System.out.println("Você não pode VOTAR.");
+        }
+        if ( idade == 16 || idade == 17 || idade >= 70) {
+            System.out.println("Seu VOTO é facultativo.");
+        } else {
+            System.out.println("Seu VOTO é obrigatório.");
+        }
+
+        entrada.close();
+    }
+}
