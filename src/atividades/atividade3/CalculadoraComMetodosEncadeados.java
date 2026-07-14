eaa016849abec70c6535f85ff5765ec649d6f7a1
Gabriel Marques <ghabrielmarquess@gmail.com>
2026-07-14 17:35:03 -0300

    Criando atividade Calculadora com Métodos Encadeados

---
+++ src/atividades/atividade3/CalculadoraComMetodosEncadeados.java
@@ -0,0 +1,28 @@
+package atividades.atividade3;
+
+import java.util.Scanner;
+
+public class CalculadoraComMetodosEncadeados {
+    static Scanner entrada = new Scanner(System.in);
+
+    public static void subtrairNumeros(int x, int y) {
+        int resultado = x - y;
+        return resultado;
+    }
+
+    public static void imprimirResultado(){
+        System.out.println(subtrairNumeros(10, 5);
+    }
+
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println();
+        imprimirResultado();
+
+
+
+
+        entrada.close();
+    }
+}
