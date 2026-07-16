6444364f72d84ab9f1db42872ddefed15176cc50
Gabriel M <magicomnstr@gmail.com>
2026-07-15 21:24:40 -0300

    Criação da Quinta Aula

---
+++ src/aulas/QuintaAula.java
@@ -0,0 +1,11 @@
+package aulas;
+
+public class QuintaAula {
+    public static void main(String[] args) {
+        int[] numeros = new int[10];
+
+        for (int numero : numeros) {
+            System.out.println(numero);
+        }
+    }
+}
