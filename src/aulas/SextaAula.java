88c7042bab5d493ad90e794ab74d8d934cdd5b85
Gabriel M <magicomnstr@gmail.com>
2026-07-20 20:20:00 -0300

    Criação da sexta aula

---
+++ src/aulas/SextaAula.java
@@ -0,0 +1,23 @@
+package aulas;
+
+import aulas.objeto.Pessoa;
+
+public class SextaAula {
+    public static void main(String[] args) {
+        Pessoa pessoa1 = new Pessoa(
+                "João",
+                2004,
+                'M',
+                "Branco",
+                "Professor"
+        );
+
+        System.out.println(pessoa1.getNome());;
+
+        System.out.println(pessoa1);
+
+        pessoa1.setNome("Fulano de tal");
+
+        System.out.println(pessoa1);
+    }
+}
