07c9036ed841817c733ad85f673011f8a85756af
Gabriel M <magicomnstr@gmail.com>
2026-07-21 21:45:41 -0300

    Criação da class Main

--- src/aulas/encapsulamento/banco/principal/Main.java
+++ src/aulas/encapsulamento/banco/principal/Main.java
@@ -10,7 +10,20 @@ public class Main {
 
         minhaConta.depositar(100);
 
-        ContaBancaria pessoa1 = new ContaBancaria("Fulano");
-        ContaBancaria pessoa2 = new ContaBancaria("Robso");
+        ContaBancaria pessoa1 = new ContaBancaria("Gabriel");
+        ContaBancaria pessoa2 = new ContaBancaria("Marcos");
+        ContaBancaria pessoa3 = new ContaBancaria("Isabella");
+
+        pessoa1.depositar(100);
+        pessoa2.sacar(1000);
+        pessoa3.depositar(100);
+        pessoa3.sacar(10);
+
+        System.out.println("A pessoa "+pessoa1.getTitular()+" depositou ");
+        pessoa1.depositar(100);
+
+        System.out.println(pessoa1);
+        System.out.println(pessoa2);
+        System.out.println(pessoa3);
     }
 }
